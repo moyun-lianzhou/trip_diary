09769a83a944f7c3e3201ab398739cb299b1d1eb
刘诚可 <1606941916@qq.com>
2025-05-08 23:19:42 +0800

    表单验证完成，用户登录获取个人信息完成

--- trip_diary_client/pages/login/login.js
+++ trip_diary_client/pages/login/login.js
@@ -1,115 +1,129 @@
 import request from '~/api/request';
 
 Page({
-  data: {
-      usernameTip:'',
-      passwordTip:'',
-    passwordText: '密\u3000码',
-    phoneNumber: '',
-    isPhoneNumber: false,
-    isCheck: false,
-    isSubmit: false,
-    isPasswordLogin: true,
-    passwordInfo: {
-      account: '',
-      password: '',
+    data: {
+        usernameTip: '',
+        passwordTip: '',
+        passwordText: '密\u3000码',
+        phoneNumber: '',
+        isPhoneNumber: false,
+        isSubmit: false,
+        isPasswordLogin: true,
+        formInfo: {
+            username: '',
+            password: '',
+        },
+        isCheck: false
     },
-    radioValue: '',
-  },
-
-  /* 自定义功能函数 */
-  changeSubmit() {
-    if (this.data.isPasswordLogin) {
-      if (this.data.passwordInfo.account !== '' && this.data.passwordInfo.password !== '' && this.data.isCheck) {
-        this.setData({ isSubmit: true });
-      } else {
-        this.setData({ isSubmit: false });
-      }
-    } else if (this.data.isPhoneNumber && this.data.isCheck) {
-      this.setData({ isSubmit: true });
-    } else {
-      this.setData({ isSubmit: false });
-    }
-  },
-
-  // 手机号变更
-  onPhoneInput(e) {
-    const isPhoneNumber = /^[1][3,4,5,7,8,9][0-9]{9}$/.test(e.detail.value);
-    this.setData({
-      isPhoneNumber,
-      phoneNumber: e.detail.value,
-    });
-    this.changeSubmit();
-  },
-
-  // 用户协议选择变更
-  onCheckChange(e) {
-    const { value } = e.detail;
-    this.setData({
-      radioValue: value,
-      isCheck: value === 'agree',
-    });
-    this.changeSubmit();
-  },
-
-  onAccountChange(e) {
-      // 用户名验证：6位以上且包含字母和数字
-  const usernameRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
-  if (!usernameRegex.test(e.detail.value)) {
-      this.setData({usernameTip:'用户名需6位以上且包含字母数字'})
-      this.usernameTip = '用户名需6位以上且包含字母数字'
-    // wx.showToast({
-    //   title: '用户名需6位以上且包含字母数字',
-    //   icon: 'none'
-    // });
-    return;
-  }else{
-    this.setData({usernameTip:''})
-  }
-    // console.log(e.detail)
-    this.setData({ passwordInfo: { ...this.data.passwordInfo, account: e.detail.value } });
-    // this.changeSubmit();
-  },
-
-  onPasswordChange(e) {
-    // 密码验证：6位以上
-  if (e.detail.value.length <= 6) {
-    this.setData({passwordTip:'密码需6位以上'})
-    // wx.showToast({
-    //   title: '密码需6位以上',
-    //   icon: 'none'
-    // });
-    // return;
-  }else{
-    this.setData({passwordTip:''})
-  }
-    this.setData({ passwordInfo: { ...this.data.passwordInfo, password: e.detail.value } });
-    // this.changeSubmit();
-  },
-
-  // 切换登录方式
-  changeLogin() {
-    this.setData({ isPasswordLogin: !this.data.isPasswordLogin, isSubmit: false });
-  },
-
-  async login() {
-    // 用户名密码登录
-    if (this.data.isPasswordLogin) {
-      const res = await request('/login/postPasswordLogin', 'post', { data: this.data.passwordInfo });
-      if (res.success) {
-        await wx.setStorageSync('access_token', res.data.token);
-        wx.switchTab({
-          url: `/pages/my/index`,
+    // 手机号变更
+    onPhoneInput(e) {
+        const isPhoneNumber = /^[1][3,4,5,7,8,9][0-9]{9}$/.test(e.detail.value);
+        this.setData({
+            isPhoneNumber,
+            phoneNumber: e.detail.value,
         });
-      }
-    } else {
-        // 手机号验证码登录
-      const res = await request('/login/getSendMessage', 'get');
-      if (res.success) {
-        wx.navigateTo({
-          url: `/pages/loginCode/loginCode?phoneNumber=${this.data.phoneNumber}`,
+        this.changeSubmit();
+    },
+    // 用户协议选择变更
+    onCheckChange() {
+        const isCheck = !this.data.isCheck
+        this.setData({
+            isCheck: isCheck
         });
-      }
-    }
-  },
+        console.log(this.data.isCheck)
+    },
+    onAccountChange(e) {
+        // 用户名验证：6位以上且包含字母和数字
+        const usernameRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+        if (!usernameRegex.test(e.detail.value)) {
+            this.setData({ usernameTip: '用户名需6位以上且包含字母数字' })
+            return;
+        }
+        this.setData({ usernameTip: '' })
+        this.setData({ 'formInfo.username': e.detail.value });
+    },
+    onPasswordChange(e) {
+        // 密码验证：6位以上
+        if (e.detail.value.length < 6) {
+            this.setData({ passwordTip: '密码需6位及以上' })
+            return
+        }
+        this.setData({ passwordTip: '' })
+        this.setData({ 'formInfo.password': e.detail.value });
+    },
+    // 切换登录方式
+    changeLogin() {
+        this.setData({ isPasswordLogin: !this.data.isPasswordLogin, isSubmit: false });
+    },
+    //验证表单是否可提交
+    formIsValid() {
+        console.log(this.data.formInfo)
+        const { username, password } = this.data.formInfo
+        const tips = {
+            checkTip: '请勾选用户协议',
+            otherTip: '请完善表单信息'
+        }
+        if (!this.data.isCheck) {
+            wx.showToast({
+                icon: 'none',
+                duration: 2000,
+                mask: true,
+                title: tips.checkTip,
+            })
+            return false
+        }
+        if (!username || !password) {
+            wx.showToast({
+                icon: 'none',
+                duration: 2000,
+                mask: true,
+                title: tips.otherTip,
+            })
+            return false
+        }
+        return true
+    },
+
+    async login() {
+        // 表单验证是否通过
+        if (!this.formIsValid()) return
+        // 用户名密码登录
+        if (this.data.isPasswordLogin) {
+            wx.login({
+                success: async (res) => {
+                    if (!res.code) {
+                        console.error('登录失败！' + res.errMsg)
+                    }
+                    //发起网络请求
+                    try {
+                        const result = await request('/auth/login', 'POST', { data: this.data.formInfo, code: res.code });
+                        // 设置token
+                        wx.setStorageSync('auth_token', result.data.auth_token);
+                        wx.setStorageSync('userId', result.data.userInfo._id);
+                        setTimeout(() => {
+                            wx.switchTab({
+                                url: `/pages/my/index`,
+                                success: () => {
+                                    console.log('页面跳转成功');
+                                },
+                                fail: (err) => {
+                                    console.error('页面跳转失败:', err);
+                                }
+                            });
+                        }, 1000);
+                    } catch (error) {
+                        console.error('网络请求出错:', error);
+                    }
+                }
+            })
+        } else {
+            // 手机号验证码登录
+            const res = await request('/login/getSendMessage', 'get');
+            if (res.success) {
+                wx.navigateTo({
+                    url: `/pages/loginCode/loginCode?phoneNumber=${this.data.phoneNumber}`,
+                });
+            }
+        }
+    },
 });
--- trip_diary_client/pages/my/index.js
+++ trip_diary_client/pages/my/index.js
@@ -6,6 +6,14 @@ import useToastBehavior from '~/behaviors/useToast';
 Page({
     behaviors: [useToastBehavior],
     data: {
+        userInfo: {
+            username: '',
+            nickname: '未登录',
+            avatarUrl: '/static/default_avatar.png',
+            tip: '用户太懒，暂无介绍',
+            gender: 0,
+            city:'未知'
+        },
         avatarUrl: '/static/default_avatar.png',
         isLogin: false,
         personalInfo: {},
@@ -56,59 +64,43 @@ Page({
             avatarUrl,
         })
     },
-
-    onLoad() {
-        // 
+    async getUserInfo() {
+        const userId = wx.getStorageSync('userId')
+        console.log(userId)
+        const userInfo = await request('/user/info', 'GET', {
+            userId
+        }).then((res) => res.data.userInfo);
+        return userInfo
     },
+    onLoad() {
 
-    async onShow() {
-        const Token = wx.getStorageSync('access_token');
-        const personalInfo = await this.getPersonalInfo();
-
-        if (Token) {
+    },
+    onShow() {
+        const token = wx.getStorageSync('auth_token');
+        if (token) {
             this.setData({
-                isLoad: true,
-                personalInfo,
+                isLogin: true,
             });
+            this.getUserInfo().then(res => {
+                console.log('sb', res)
+                this.setData({
+                    userInfo: {
+                        username: res.username,
+                        nickname: res.nickname,
+                        avatarUrl: res.avatarUrl,
+                        tip: res.tip,
+                        gender: res.gender,
+                        city: '未知'
+                    }
+                })
+            })
         }
     },
-
-    async getPersonalInfo() {
-        const info = await request('/api/genPersonalInfo').then((res) => res.data.data);
-        return info;
-    },
-
     onLogin(e) {
         console.log('sb')
         wx.navigateTo({
             url: '/pages/login/login',
-          })
-        // wx.login({
-        //     timeout: 3000,
-        //     success(res) {
-        //         if (res.code) {
-        //             //发起网络请求
-        //             wx.request({
-        //                 url: 'http://localhost:3000/api/auth/login',
-        //                 method: 'POST',
-        //                 data: {
-        //                     code: res.code
-        //                 },
-        //                 success(res) {
-        //                     console.log('登录成功', res.data)
-        //                     this.isLogin = true
-        //                     const {openid, userInfo} = res.data
-        //                     console.log(openid, userInfo)
-        //                 }
-        //             })
-        //         } else {
-        //             console.log('登录失败！')
-        //         }
-        //     },
-        //     fail(errMsg, errno) {
-        //         console.log('接口调用失败111', errMsg, errno)
-        //     }
-        // })
+        })
     },
 
     onNavigateTo() {
